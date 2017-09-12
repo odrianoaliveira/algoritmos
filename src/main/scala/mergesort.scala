4de0914d541ea2dadcb375650357aabb1d8be60e
Adriano Oliveira <aso.rio@gmail.com>
2017-09-12 13:42:26 -0300

    Update mergesort.scala
    fix it

--- src/main/scala/mergesort.scala
+++ src/main/scala/mergesort.scala
@@ -1,19 +1,26 @@
+import math.Ordering
+
 object mergesort {
 
-  def msort(xs: List[Int]): List[Int] = {
+  def msort[T](xs: List[T])(implicit ord: Ordering[T]): List[T] = {
     val n = xs.length / 2
     if (n == 0) xs
     else {
-      def merge(xs: List[Int], ys: List[Int]) = (xs, ys) match {
+      def merge(xs: List[T], ys: List[T]): List[T] = (xs, ys) match {
         case (Nil, ys) => ys
         case (xs, Nil) => xs
-        case ( x :: xs1, y :: ys1 ) =>
-          if (x < y) x :: merge(xs1, ys1)
-          else y :: merge(xs, ys)
+        case (x :: xs1, y :: ys1) =>
+          if (ord.lt(x, y)) x :: merge(xs1, ys)
+          else y :: merge(xs, ys1)
       }
 
       val (fst, snd) = xs splitAt n
-      merge(msort(fst), msort(snd))
+      merge(msort(fst), msort(snd)
     }
   }
+
+  val nums = List(2, -4, 5, 7, 1)
+  val fruits = List("apple", "pinapple", "orange", "banana")
+  msort(nums)
+  msort(fruits)
 }
